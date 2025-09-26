5ba00ffcb6b3635b7b6280b4ad549e2acb2abd86
Marius <marius-sol@pm.me>
2025-09-26 17:32:34 +0200

    update kubeenv

--- kubeenv.sh
+++ kubeenv.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-assets="./assets"
-bin="./bin"
+basedir="$(cd "$(dirname $(realpath "${BASH_SOURCE[0]}"))" >/dev/null 2>&1 && pwd)"
 
-os=$(uname)
+assets="${basedir}/assets"
+bin="${basedir}/bin"
+
+os="$(uname)"
 
 hardware=$(uname -m)
 declare -A hardware_map
@@ -11,14 +13,14 @@ hardware_map["x86_64"]="amd64"
 hardware_map["arm64"]="arm64"
 
 function main() {
-    source log.sh
-    source kubeenv_software.sh
-    
-    if [[ ! -d ${bin} ]]; then
-        mkdir -p ${bin}
+    source "${basedir}/log.sh"
+    source "${basedir}/kubeenv_software.sh"
+
+    if [[ ! -d "${bin}" ]]; then
+        mkdir -p "${bin}"
     fi
-    if [[ ! -d ${assets} ]]; then
-        mkdir -p ${assets}
+    if [[ ! -d "${assets}" ]]; then
+        mkdir -p "${assets}"
     fi
 
     gh_install
@@ -26,27 +28,32 @@ function main() {
     jq_install
     k9s_install
     helm_install
+    krew_install
     kubens_install
     kubectl_install
-    kompose_install
     kubectx_install
     kubeseal_install
-    kustomize_install
-
+    headlamp_install
 }
 
 function download() {
     # download <url>
+    if [[ -z "$(command -v wget)" ]];then
+        log warn "wget is not installed, install with 'sudo apt install wget'"
+        exit 1
+    fi
+
     url="${1}"
     path="${assets}"
     filename="$(basename ${url})"
     if [[ -f "${assets}/${filename}" ]]; then
-        log Notice "${filename} does already exists" 
+        log Notice "${filename} does already exists"
         return
     fi
     log info "Downloading ${filename} from ${url} to ${path}"
-    wget -P "$assets" -q "$url" 2>&1
-    if [[ $? -gt 0 ]]; then
+    wget -P "${assets}" -q "$url" 2>&1
+    rc="${?}"
+    if [[ "${rc}" -gt 0 ]]; then
         log error "Downloading ${filename} failed"
         return
     fi
@@ -55,7 +62,7 @@ function download() {
 function unpack() {
     # unpack <download_url> <sublevel> <files>
     archive="${assets}/$(basename $1)"
-    sublevel=${2}
+    sublevel="${2}"
     files="${@:3}"
     if [[ ! -f ${archive} ]]; then
         log error "${archive} does not exsist; Was it successfully downloaded?"
@@ -63,19 +70,20 @@ function unpack() {
     fi
     if [[ -f "${bin}/{${files}}" ]]; then
         log notice "${files} has already been packed out"
-        return  
+        return
     fi
     case "${archive}" in
         *.tar.gz)
-            tar --strip-components=${sublevel} -xzf "${archive}" -C "${bin}" ${files}
+            tar --strip-components="${sublevel}" -xzf "${archive}" -C "${bin}" "${files}" --warning=no-unknown-keyword
+            rc="${?}"
         ;;
         *)
             log error "Not an know archive format"
+            return
         ;;
     esac
-    
 
-    if [[ $? -gt 0 ]]; then
+    if [[ "${rc}" -gt 0 ]]; then
         log warn "Not possible to unarchive ${archive}"
         return
     else
@@ -84,62 +92,62 @@ function unpack() {
 }
 
 function kubectl_install() {
-    download ${kubectl_download_url}
-    cp ${assets}/$(basename ${kubectl_download_url}) ${bin}
-    chmod +x ${bin}/$(basename ${kubectl_download_url})
+    download "${kubectl_download_url}"
+    cp "${assets}/$(basename ${kubectl_download_url})" "${bin}"
+    chmod +x "${bin}/$(basename ${kubectl_download_url})"
 }
 
 function yq_install() {
-    download ${yq_download_url}
-    unpack ${yq_download_url} 0 ./yq_${os,,}_${hardware_map[${hardware}]}
-    mv ${bin}/yq_${os,,}_${hardware_map[${hardware}]} ${bin}/yq
+    download "${yq_download_url}"
+    unpack "${yq_download_url}" 0 "./yq_${os,,}_${hardware_map[${hardware}]}"
+    mv "${bin}/yq_${os,,}_${hardware_map[${hardware}]}" "${bin}/yq"
 }
 
 function gh_install() {
-    download ${gh_download_url}
-    unpack ${gh_download_url} 2 gh_${gh_version}_${os,,}_${hardware_map[${hardware}]}/bin/gh
+    download "${gh_download_url}"
+    unpack "${gh_download_url}" 2 "gh_${gh_version}_${os,,}_${hardware_map[${hardware}]}/bin/gh"
 }
 
 function jq_install() {
-    download ${jq_download_url}
-    mv ${assets}/jq-${os,,}-${hardware_map[${hardware}]} ${bin}/jq
-    chmod +x ${bin}/jq
+    download "${jq_download_url}"
+    mv "${assets}/jq-${os,,}-${hardware_map[${hardware}]}" "${bin}/jq"
+    chmod +x "${bin}/jq"
 }
 
 function k9s_install() {
-    download ${k9s_download_url}
-    unpack ${k9s_download_url} 0 k9s
+    download "${k9s_download_url}"
+    unpack "${k9s_download_url}" 0 k9s
+}
+
+function krew_install() {
+    download "${krew_download_url}"
+    unpack "${krew_download_url}" 0 "./krew-${os,,}_${hardware_map[${hardware}]}"
+    mv "${bin}/krew-${os,,}_${hardware_map[${hardware}]}" "${bin}/krew"
 }
 
 function helm_install() {
-    download ${helm_download_url}
-    unpack ${helm_download_url} 1 "${os,,}-${hardware_map[${hardware}]}/helm"
+    download "${helm_download_url}"
+    unpack "${helm_download_url}" 1 "${os,,}-${hardware_map[${hardware}]}/helm"
 }
 
 function kubectx_install() {
-    download ${kubectx_download_url}
-    unpack ${kubectx_download_url} 0 kubectx
+    download "${kubectx_download_url}"
+    unpack "${kubectx_download_url}" 0 kubectx
 }
 
 function kubens_install() {
-    download ${kubens_download_url}
-    unpack ${kubens_download_url} 0 kubens
+    download "${kubens_download_url}"
+    unpack "${kubens_download_url}" 0 kubens
 }
 
 function kubeseal_install() {
-    download ${kubeseal_download_url}
-    unpack ${kubeseal_download_url} 0 kubeseal
+    download "${kubeseal_download_url}"
+    unpack "${kubeseal_download_url}" 0 kubeseal
 }
 
-function kustomize_install() {
-    download ${kustomize_download_url}
-    unpack ${kustomize_download_url} 0 kustomize
-}
-
-function kompose_install() {
-    download ${kompose_download_url}
-    mv ${assets}/kompose-${os,,}-${hardware_map[${hardware}]} ${bin}/kompose
-    chmod +x ${bin}/kompose
+function headlamp_install() {
+    download "${headlamp_download_url}"
+    unpack "${headlamp_download_url}" 1 "Headlamp-${headlamp_version}-${os,,}-x64/headlamp"
 }
 
 main
--- kubeenv_update.sh
+++ kubeenv_update.sh
@@ -1,29 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+basedir="$(cd "$(dirname $(realpath "${BASH_SOURCE[0]}"))" >/dev/null 2>&1 && pwd)"
 
 function main() {
-    source log.sh
-    source kubeenv_software.sh
+    source "${basedir}/log.sh"
+    source "${basedir}/kubeenv_software.sh"
 
     github_release_tag "yq"
     github_release_tag "jq"
     github_release_tag "gh"
     github_release_tag "k9s"
     github_release_tag "helm"
+    github_release_tag "krew"
     github_release_tag "kubens"
     github_release_tag "kubectx"
-    github_release_tag "kompose"
     github_release_tag "kubeseal"
-    github_release_tag "kustomize"
+    github_release_tag "headlamp"
 }
 
 function github_release_tag() {
-    software_name=${1}
+    software_name="${1}"
     version_var="${software_name}_version"
     current_version="${!version_var}"
     repo_var="${software_name}_version_page"
-    org_repo=${!repo_var}
-    latest=$(curl -sL https://api.github.com/repos/${org_repo}/releases/latest | jq -r ".tag_name" | awk -F'/' '{print $NF}' | sed 's/^v//')
-    if [[ ${?} -gt 0 || ${latest} == "null" ]];then
+    org_repo="${!repo_var}"
+    latest=$(curl -sL https://api.github.com/repos/${org_repo}/releases/latest | grep tag_name | awk -F'"' '{gsub(/^v/, "", $4); print $4}')
+    rc=${?}
+    if [[ "${rc}" -gt 0 || "${latest}" == "null" || "${latest}" == "" ]];then
         log error "Could not fetch Github version from ${org_repo}"
         return
     fi
@@ -34,7 +37,6 @@ function github_release_tag() {
     fi
 
     update_version "${software_name}" "${latest}"
-    log info "${software_name} has been updated from '${current_version}' -> '${latest}'"
 }
 
 function update_version() {
@@ -42,13 +44,22 @@ function update_version() {
     version_var="${software_name}_version"
     current_version="${!version_var}"
     latest="${2}"
-    
-    sed -i "s|${software_name}_version=\"${current_version}\"|${software_name}_version=\"${latest}\"|g" kubeenv_software.sh
-    if [[ $? -gt 0 ]]; then
+
+    rc=0
+    if [[ "$(uname)" == "Linux" ]]; then
+        sed -i "s|${software_name}_version=\"${current_version}\"|${software_name}_version=\"${latest}\"|g" "${basedir}/kubeenv_software.sh"
+        rc="${?}"
+    elif [[ "$(uname)" == "Darwin" ]]; then
+        # macOS (BSD sed) requires an explicit backup suffix; use empty string ''
+        sed -i '' "s|${software_name}_version=\"${current_version}\"|${software_name}_version=\"${latest}\"|g" "${basedir}/kubeenv_software.sh"
+        rc="${?}"
+    fi
+
+    if [[ "${rc}" -gt 0 ]]; then
         log error "Something went wrong updating version for ${software_name}"
         return
     fi
-    log notice "${software_name} has been update to version ${latest}"
+    log notice "${software_name} has been update to version ${latest} from ${current_version}"
 }
 
 main
